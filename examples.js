76740c9202b7e98b665b1190a4631e4c5b16f175
Dudys <dudys.michal@gmail.com>
2020-03-02 13:35:59 +0100

    Purple rain - change function Drop to class

--- examples.js
+++ examples.js
@@ -505,31 +505,33 @@ var purpleRain = function(p) {
       }
     }
 
-    p.Drop = function() {
-      this.x = p.random(p.width);
-      this.y = p.random(-500, -50);
-      this.z = p.random(0, 20);
-      this.len = p.map(this.z, 0, 20, 10, 20);
-      this.yspeed = p.map(this.z, 0, 20, 1, 20);
-
-      this.fall = function() {
-        this.y = this.y + this.yspeed;
-        var grav = p.map(this.z, 0, 20, 0, 0.2);
-        this.yspeed = this.yspeed + grav;
-
-        if (this.y > p.height) {
-          this.y = p.random(-200, -100);
-          this.yspeed = p.map(this.z, 0, 20, 4, 10);
+    class Drop {
+        constructor () {
+            this.x = p.random(p.width);
+            this.y = p.random(-500, -50);
+            this.z = p.random(0, 20);
+            this.len = p.map(this.z, 0, 20, 10, 20);
+            this.yspeed = p.map(this.z, 0, 20, 1, 20);
         }
-      };
+     
+        fall = function() {
+            this.y = this.y + this.yspeed;
+            var grav = p.map(this.z, 0, 20, 0, 0.2);
+            this.yspeed = this.yspeed + grav;
 
-      this.show = function() {
-        var thick = p.map(this.z, 0, 20, 1, 3);
-        p.strokeWeight(thick);
-        p.stroke(138, 43, 226);
-        p.line(this.x, this.y, this.x, this.y + this.len);
+            if (this.y > p.height) {
+              this.y = p.random(-200, -100);
+              this.yspeed = p.map(this.z, 0, 20, 4, 10);
+            }
+        };
+
+        show = function() {
+            var thick = p.map(this.z, 0, 20, 1, 3);
+            p.strokeWeight(thick);
+            p.stroke(138, 43, 226);
+            p.line(this.x, this.y, this.x, this.y + this.len);
       };
-    }    
+    }  
 }
 
 let myp5_9 = new p5(purpleRain, "c9");
